52873470c065f9dc56b9b1da315b8c72c0cdcfe3
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-15 12:22:12 +0500

    Week -02 is in progress

--- Week02_playerjumpinggame/src/Game/Game.tsx
+++ Week02_playerjumpinggame/src/Game/Game.tsx
@@ -9,7 +9,11 @@ const Game: React.FC = () => {
   const gamref = useRef<HTMLDivElement>(null);
   const PhaserGameRef = useRef<Phaser.Game | null>(null);
 
-  
+  useEffect(()=>{
+    if(gameref && !PhaserGameRef.current){
+      
+    }
+  },[])
   return (
     <>
       <div></div>
