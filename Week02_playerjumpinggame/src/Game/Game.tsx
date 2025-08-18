fda61c2ed69da8461e8310c8fefbe33aac83ef69
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-18 12:15:36 +0500

    Week02 in progress

--- Week02_playerjumpinggame/src/Game/Game.tsx
+++ Week02_playerjumpinggame/src/Game/Game.tsx
@@ -1,7 +1,19 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Footer, GameWrapper, Header } from "./Game.style";
 
 const Game = () => {
+  const { gameref } = useRef<HTMLDivElement>(null);
+  const { phasergameref } = useRef<Phaser.Game>(null);
+  const [score, setscore] = useState<number>(0);
+  const [lives, setlives] = useState<number>(3);
+
+
+  useEffect(() => {
+
+    if(gameref && !phasergameref){
+      
+    }
+  }, []);
   return (
     <>
       <Header>Score: </Header>
